2b8079306f035520d09c3eb6b5ff5deaeb63d56a
alireza1385mamani <85825573+alireza1385mamani@users.noreply.github.com>
2024-04-04 10:39:31 +0330

    Initial commit

---
+++ bjSettings/paGlobals.py
@@ -0,0 +1,8 @@
+pac_curr_lang = "lang1"
+commands_file = "commands.ini"
+gestures_file = "gestures.ini"
+bluetooth_device_name = "PactosNTArm"
+search_bluetooth = False
+lang1 = "pactos-fa-ir-g1.utb"
+lang2 = "pactos-en-us-g1.ctb"
+device_model = "bj_hooshmand"
